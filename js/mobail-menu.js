58e68492b4f3cd96a28c509de7c05aed50ce1bb3
maxym-huklivsky <106371809+maxym-huklivsky@users.noreply.github.com>
2022-08-30 15:14:10 +0300

    add close link in menu

--- js/mobail-menu.js
+++ js/mobail-menu.js
@@ -1,12 +1,19 @@
 (() => {
 	const menuOpenBtn = document.querySelector('[data-menu-open]');
 	const menuCloseBtn = document.querySelector('[data-menu-close]');
+	const menuCloseBtnq = document.querySelector('[data-menu-closeq]');
+	const menuCloseBtnw = document.querySelector('[data-menu-closew]');
+	const menuCloseBtne = document.querySelector('[data-menu-closee]');
 
 	const mobileMenu = document.querySelector('[data-menu]');
 	const body = document.querySelector('body');
 
 	menuOpenBtn.addEventListener('click', openModal);
 	menuCloseBtn.addEventListener('click', openModal);
+	menuCloseBtnq.addEventListener('click', openModal);
+	menuCloseBtnw.addEventListener('click', openModal);
+	menuCloseBtne.addEventListener('click', openModal);
+
 
 	function openModal() {
 		mobileMenu.classList.toggle('is-open');
